071b9c0e06b534f880314dc097d671b522f4b185
= <if22b198@technikum-wien.at>
2023-09-24 16:09:39 +0200

    - now here is the server actually :p

---
+++ server/src/main/java/at/technikum/swkomsemesterproject/api/ApiApiController.java
@@ -0,0 +1,27 @@
+package at.technikum.swkomsemesterproject.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-09-21T18:51:22.691331Z[Etc/UTC]")
+@Controller
+@RequestMapping("${openapi.mockServer.base-path:}")
+public class ApiApiController implements ApiApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public ApiApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
