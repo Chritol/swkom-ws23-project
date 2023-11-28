974122b692691443651d9f9165f48b67fb5bf1e3
= <if22b198@technikum-wien.at>
2023-11-28 20:03:23 +0100

    implemented ocr application

---
+++ paperlessOCR/src/main/java/org/openapitools/paperlessocr/constants/RabbitMqConstants.java
@@ -0,0 +1,6 @@
+package org.openapitools.paperlessocr.constants;
+
+public class RabbitMqConstants {
+    public static final String DOCUMENT_IN_QUEUE = "DOCUMENT_IN_QUEUE";
+    public static final String DOCUMENT_OUT_QUEUE = "DOCUMENT_OUT_QUEUE";
+}
---
+++ paperlessOCR/src/test/java/org/openapitools/paperlessocr/PaperlessOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openapitools.paperlessocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperlessOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
